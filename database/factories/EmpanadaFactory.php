81424bc53271c6810832b1321edc21ccfbe24be0
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-06-30 12:00:34 -0300

    Limpieza

--- database/factories/EmpanadaFactory.php
+++ database/factories/EmpanadaFactory.php
@@ -21,17 +21,4 @@ class EmpanadaFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
 }
