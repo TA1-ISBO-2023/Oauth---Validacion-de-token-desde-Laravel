77ca8261a3963dfe6a2b21571ad5f5472efd905a
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2023-10-12 21:59:31 -0300

    Agregado soporte a memcachec

--- app/Http/Middleware/Autenticacion.php
+++ app/Http/Middleware/Autenticacion.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 
 class Autenticacion
@@ -19,14 +20,20 @@ class Autenticacion
     public function handle(Request $request, Closure $next)
     {
 
-        $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
+        $token = explode(" ", $request -> header("Authorization"))[1];
+        if(Cache::has($token)){
+            return $next($request);
+        }
 
+        $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
         $response = Http::withHeaders($tokenHeader)->get(getenv("API_AUTH_URL") . "/validate");
 
-        
-        if($response -> successful())
+
+        if($response -> successful()){
+            Cache::put($token , $response -> json(), 500);
             return $next($request);
-        
+        }
+
         return response(['message' => 'Not Allowed'], 403);
 
 
